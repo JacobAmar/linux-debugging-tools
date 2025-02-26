a2a99987f6f6697ebc742d36ace28b8331f69527
JacobAmar <yaakov.amar@unity3d.com>
2025-02-26 15:54:15 +0200

    Fix index for device reads in iochecker.py

--- iochecker.py
+++ iochecker.py
@@ -21,7 +21,7 @@ def analyze_peaks(device_name, threshold):
         
         if len(fields) > 0 and fields[0] == device_name:
             try:
-                reads = float(fields[3])
+                reads = float(fields[1])
                 print (f"Reads: {reads}")
                 current_time = datetime.now()
                 
